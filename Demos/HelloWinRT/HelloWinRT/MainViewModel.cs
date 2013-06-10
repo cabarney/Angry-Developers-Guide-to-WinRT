5ae98ac41d9bd3b4f6da45653c8d1b3d64181486
Adam Barney <adam@adambarney.com>
2013-06-10 07:24:35 -0500

    Improve consistency between demos and slides

--- Demos/HelloWinRT/HelloWinRT/MainViewModel.cs
+++ Demos/HelloWinRT/HelloWinRT/MainViewModel.cs
@@ -4,10 +4,6 @@ namespace HelloWinRT
 {
     public class MainViewModel : ViewModelBase
     {
-        public MainViewModel()
-        {
-            Number = 44;
-        }
         private double? _number;
         [Range(0, 100, ErrorMessage = "Number must be between 0-100")]
         public double? Number
